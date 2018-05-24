ddb6037a373ea7f80209c8a015adaeae0a16990c
Muhammad Muizzsuddin <arc.muiss@yahoo.com>
2018-05-24 14:34:24 +0700

    Update Config

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+    # MySQL configurations
+    MYSQL_DATABASE_USER = 'default'
+    MYSQL_DATABASE_PASSWORD = 'default'
+    MYSQL_DATABASE_DB = 'default'
+    MYSQL_DATABASE_HOST = 'localhost'
--- db/__init__.py
+++ db/__init__.py
@@ -2,11 +2,4 @@ from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Mszs2153@'
-app.config['MYSQL_DATABASE_DB'] = 'ItemListDb'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-
 mysql.init_app(app)
